9d373e7bdd6ed76d064d53e2691ab93c2a63aa20
Ahmad <ahmad.hazara@gmx.ch>
2019-12-09 00:17:06 +0100

    Adding the state method to the Class and using it inside the Persons Component.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello React</h1>
-      <p>This is really Working!</p>
-      <Person name="Max" age="24" />
-      <Person name="James" age="34">
-        Hi i'm the Child
-      </Person>
-      <Person name="Joe" age="41" />
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Yas', age: 44 },
+      { name: 'Gabriel', age: 33 },
+      { name: 'Ali', age: 20 }
+    ]
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello React</h1>
+        <p>This is really Working!</p>
+        <Person name={this.state.persons[0].name} age="24" />
+        <Person name="James" age="34">
+          Hi i'm the Child
+        </Person>
+        <Person name="Joe" age="41" />
+      </div>
+    );
+  }
 }
 
 export default App;
