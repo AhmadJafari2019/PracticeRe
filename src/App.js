72497210401a2b46c0332cdd7197a59354af0396
Ahmad <ahmad.hazara@gmx.ch>
2019-12-09 13:48:27 +0100

    JavaScript way set the Person Component Dynamic.

--- src/App.js
+++ src/App.js
@@ -45,36 +45,40 @@ class App extends Component {
       border: '2px solid blue'
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'FaFa')}
+            changed={this.nameChangeHandler}
+          >
+            Hi i'm the Child!
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hello React</h1>
         <p>This is really Working!</p>
-        <button
-          onClick={this.togglePersonHandler}
-          style={style}
-        >
+        <button onClick={this.togglePersonHandler} style={style}>
           Switch Me
         </button>
-        {this.state.showPersons ? (
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-            />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'FaFa')}
-              changed={this.nameChangeHandler}
-            >
-              Hi i'm the Child!
-            </Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-            />
-          </div>
-        ) : null}
+
+        {persons}
       </div>
     );
   }
