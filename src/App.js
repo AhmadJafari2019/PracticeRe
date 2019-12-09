24ef666ba5965875632811c86dfdd402f8a34b20
Ahmad <ahmad.hazara@gmx.ch>
2019-12-09 22:39:41 +0100

    Import the Radium librarie and wrap the App Component with it.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import './Person/Person.css';
 import Person from './Person/Person';
 
@@ -88,4 +89,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
