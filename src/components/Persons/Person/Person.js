b64a33fc7aa4fc6dea85765d4c8d35ddaa391197
Ahmad <ahmad.hazara@gmx.ch>
2019-12-10 17:40:20 +0100

    A better structure Components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -20,7 +20,13 @@ const person = props => {
         I am {props.name} and I am {props.age} years old
       </p>
       <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
+      <input
+        type="text"
+        onChange={props.changed}
+        value={props.name}
+       
+       
+      />
     </StyledDiv>
   );
 };
