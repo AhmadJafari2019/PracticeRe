685e7a80b140d6d07e8651eea0453aa1811e4b5d
Ahmad <ahmad.hazara@gmx.ch>
2019-12-12 12:36:08 +0100

    Higher order Components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,17 +6,17 @@ class Person extends Component {
   render() {
     console.log('[Person.js] rendering... ');
     return (
-      <Aux>
+      <Aux className={classes.Person}>
         {/* /<div className={classes.Person}> */}
-          <p onClick={this.props.click}>
-            I am {this.props.name} and I am {this.props.age} years old
-          </p>
-          <p>{this.props.children}</p>
-          <input
-            type="text"
-            onChange={this.props.changed}
-            value={this.props.name}
-          />
+        <p onClick={this.props.click}>
+          I am {this.props.name} and I am {this.props.age} years old
+        </p>
+        <p>{this.props.children}</p>
+        <input
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
         {/* </div> */}
       </Aux>
     );
